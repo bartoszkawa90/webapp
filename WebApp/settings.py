bde2078481340b8f9b923e1796dfd60492f9d054
Bartosz_Kawa <bartekkawa2@gmail.com>
2023-12-06 22:58:54 +0100

    finished

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
